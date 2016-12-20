de5b75df5bf3b73fd2f9ce56da0e3e909204c866
Thien chi <tcv.frb@gmail.com>
2016-12-20 20:49:07 +0100

    Ajout layout.php : On créer la base WEB du site

---
+++ View/layout.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+	</head>
+	<body>
+		<header>
+			<nav>
+				<ul>
+					<li> <a> ItemMenu1 </a> </li>
+					<li> <a> ItemMenu2 </a> </li>
+					<li> <a> ItemMenu3 </a> </li>
+					<li> <a> ItemMenu4 </a> </li>
+					<li> <a> ItemMenu5 </a> </li>
+				</ul>
+			</nav>
+		</header>
+	</body>
+</html>
