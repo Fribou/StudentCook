f119521f7ee59ed771c5e3396b9f9a01a7af6d77
Thien chi <tcv.frb@gmail.com>
2016-12-26 11:13:00 +0100

    Ajout UserManager.php Adapte au projet

---
+++ Model/UserManager.php
@@ -0,0 +1,16 @@
+<?php
+   class UserManager extends Model{
+	   
+   /*
+   function getUser($pseudo, $password){
+      $sql='SELECT Login, Pass, UserID
+      FROM User
+      WHERE Login = "'.$pseudo.'" and Pass = "'.$password.'"';
+      $req = $this->executeRequest($sql);
+      $results = $req->fetch(PDO::FETCH_ASSOC);
+      return $results;
+    }
+	*/
+	
+  }
+?>
