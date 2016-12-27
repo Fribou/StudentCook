9261f580142e93b9529f5c0b2a8adc12e3fc8d14
Thien chi <tcv.frb@gmail.com>
2016-12-27 12:09:51 +0100

    Ajout detailRecette.php

---
+++ View/detailRecette.php
@@ -0,0 +1,14 @@
+<?php
+	$titre=$results1['Titre'];
+	ob_start();
+	echo '<h1>' . $results['Nom'] .'</h1>';
+
+	echo $results['ingredient1'];
+	echo $results['ingredient2'];
+	echo $results['ingredient3'];
+	echo $results['ingredient4'];
+	echo $results['ingredient5'];
+	
+	$contenu=ob_get_clean();
+	require('layout.php');
+?>
