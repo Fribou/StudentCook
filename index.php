7fce5a89827c4a099f9a52fee901203666cd5820
Thien chi <tcv.frb@gmail.com>
2016-12-30 11:47:15 +0100

    Debut index.php

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php	
 	session_start();
+	require("Model/Model.php");
+	require('Model/RecetteManager.php');
+	require('Model/UserManager.php');
+	require('Model/IngredientManager.php');
+	
+	if(isset($_GET['action'])&& $_GET['action']=='deconnexion') {
+		session_destroy();
+		header ('Location: index.php');
+		exit(0);
+	}
+	
 	require("View/ingredients.php");	
 ?>
