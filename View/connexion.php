40f06d092a8e629defd5bdc4db84db376137c402
MiGirard <mihajlo.girard@hotmail.fr>
2017-01-14 22:58:54 +0000

    CSS connexion

--- View/connexion.php
+++ View/connexion.php
@@ -2,22 +2,47 @@
 $title = "StudentCook : Connexion";
 ob_start();
 
-//On regarde si l'utilisateur n'est pas déjà connecté
-echo '<h1>Connexion</h1>';
+//On regarde si l'utilisateur n'est pas dï¿½jï¿½ connectï¿½
+
 
 //On est dans la page de formulaire
 
-  echo'<p>Pour vous connecter, veuillez remplir les champs utilisateur et mot de passe suivants:</p>
+  echo'  <div class="container">
+          <div class="row main">
+            <div class="main-login main-center">
+            <h1>Connexion</h1>
+              <form class="" method="post" action="#">
 
-    <form method="post" action="./index.php?action=connexion">
-      <fieldset>
-        <legend>Connexion</legend>
-        <p>
-          <label name="pseudo">Pseudo :</label><input name="pseudo" type="text"/>
-          <label name="password">Mot de Passe :</label><input type="password" name="password" />
-        </p>
-      </fieldset>
-    <p><input type="submit" value="Connexion" /></p></form>';     
+                <div class="form-group">
+                  <label for="username" class="cols-sm-2 control-label">Pseudo</label>
+                  <div class="cols-sm-10">
+                    <div class="input-group">
+                      <span class="input-group-addon"><i class="fa fa-envelope fa" aria-hidden="true"></i></span>
+                      <input type="text" class="form-control" name="pseudo" id="identifiant"  placeholder="Entrer votre pseudo"/>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="form-group">
+                  <label for="password" class="cols-sm-2 control-label">Mot de passe</label>
+                  <div class="cols-sm-10">
+                    <div class="input-group">
+                      <span class="input-group-addon"><i class="fa fa-users fa" aria-hidden="true"></i></span>
+                      <input type="password" class="form-control" name="password" id="password"  placeholder="Entrer votre mot de passe"/>
+                    </div>
+                  </div>
+                </div>
+
+
+
+                <div class="form-group ">
+                  <a href="index.php"  type="button" id="button" class="btn btn-primary btn-lg btn-block login-button">Connexion</a>
+                </div>
+
+              </form>
+            </div>
+          </div>
+        </div>';
 
 $contenu = ob_get_clean();
 require('View/layout.php');
