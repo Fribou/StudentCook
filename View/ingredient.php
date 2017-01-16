f75ac0827640e7cd36e67888a6770f09a17e4a47
Pyke1 <tdicquemare@gmail.com>
2017-01-16 15:16:39 +0100

    ajout carotte

--- View/ingredient.php
+++ View/ingredient.php
@@ -90,13 +90,14 @@
 
 <h2> Liste des ingredients </h2>
 <p> Tous les ingredients utilises dans les recettes sont ici !</p>
-
+<div id="list3">
+   <ul>
 <?php
 	//affiche les ingredients en fonction de la base de donnee d'ingredient
 	foreach($results as $ingredient){
 		echo '<div class="'.$ingredient['typeIngredient'].'"><li>'.$ingredient['NOMINGREDIENT'].'</li></div>';
 	}
-	echo'</div>';
+	echo'</ul></div></div>';
 	$contenu=ob_get_clean();
 	require('View/layout.php');
 ?>
