624239a5b8b6ad7a9b7389a5fdf6aff7f05638bf
Pyke1 <tdicquemare@gmail.com>
2017-03-23 15:19:07 +0100

    LALAL

--- View/recette.php
+++ View/recette.php
@@ -26,52 +26,27 @@
 			</div></form>';
 	}
 
-		 
-		 /*if(isset($_POST['ajoutRecette']))
-			echo'<li><input  name= "AjoutRecette" value="Ajouter" type="submit"></input></form></li>';
-		 if(isset($_POST['proposerRecette']))
-			echo'<input  name= "ProposerRecette" value="Proposer" type="submit"></input></form>';*/
-	
-	//recherche recette
-	/*echo '	<h2>Recherche des Recettes</h2>
-
-	<form method="post" action="./index.php?action=recette">
-		 <li class="white">Duree : <input type="number" name="dureeRecette" value="0"></li>
-		 <li class="white">Origine : <select name="Origine"><option value="pas_origine">Selectionner une origine</option></li>';
-				foreach($results as $origine){
-							if(!in_array($origine['ORIGINE'], $tabOrigine)){
-								$tabOrigine[] = $origine['ORIGINE'];
-								echo '<option value="'.$origine['ORIGINE'].'">'.$origine['ORIGINE'].'</option>';
-							}
-						}*/
-//</select>
-		//echo' <li>Ingredient : <input type="text" name="ingredient1"></li>';
-
-
-
-
-
-		/*echo'<div class="hot-container">
-				<p>
-					<input class="btn btn-red" name="rechercheRecette" value="Rechercher une Recette" type="submit"></input>
-				</p>
-			</div>
-
-		</form>';*/
-
+if(empty($results)){
+		echo "<div class='texte'>Existe aucune Recette avec ces ingrédients</div>";
+	}
+	else{
 ?>
 	<h1>Liste des Recettes</h1>
 	<?php
+	
 	echo '<table class="table-fill">';
 	//affiche liste recette
 		foreach($results as $donnees){
 				echo '<tr>';
 				echo '<td><img src="./'. $donnees['IMAGE'].'" alt="tortilla" width="200" /></td>';
 				echo '<td>'.$donnees['NOMRECETTE'].'</td>';
-				if(isset($_SESSION['rechercheIngredient']) && !empty($_SESSION['rechercheIngredient']))
+				
+				if(isset($_SESSION['rechercheIngre']) and $_SESSION['rechercheIngre']== true)
 					echo '<td><a href='.'"index.php?action=ingredient&rechercheRecette='.$donnees['IDRECETTE'].'#popup1">details de la recette</a></td>';
+				
 				else
 					echo '<td><a href='.'"index.php?action=recette&recetteid='.$donnees['IDRECETTE'].'#popup1">details de la recette</a></td>';
+				
 				echo '<td><a href='.'"index.php?action=recette&incr='.$donnees['IDRECETTE'].'">Ajouter recette a votre liste</a></td>';
 				echo'<form action="index.php?action=recette" method="post">';
 				echo'<td><button type="submit" name="effacerRecette" value ="'.$donnees['IDRECETTE'].'">X</button></form></td>';
@@ -81,6 +56,8 @@
 
 	 ?>
 	 </table>
+	 
+	 
 <?php
 	//Details de la recette
 	echo '	 <div id="popup1" class="overlay">
@@ -91,7 +68,7 @@
 
 									echo '<h1>' . $result['NOMRECETTE'] .'</h1>';
 									echo '<br />';
-									echo '<img src="./'. $result['IMAGE'].'" alt="tortilla" width="300" /><br />';
+									echo '<img src="./'.$result['IMAGE'].'" alt="image" width="200" /><br />';
 									echo $result['DEFINITION'];
 									echo '<br />';
 									echo '<br />';
@@ -110,6 +87,7 @@
 									echo'</div>
 									</div>
 									</div>';
+	}
 //affiche la liste de choix de recette
 echo'<br />';
 if(isset($_SESSION['arrayRecette']) && !empty($_SESSION['arrayRecette'])){
